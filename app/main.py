4a6e42651f104ab6dc94f44eafb1949638c9b21c
Harrrryz <harry.wadu233@gmail.com>
2024-06-25 11:33:32 +0800

    add pagination

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from fastapi import FastAPI, HTTPException
-from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship  # type: ignore
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship, column, and_  # type: ignore
 from pydantic import BaseModel, field_validator
 from contextlib import asynccontextmanager
 from sqlalchemy import func
@@ -69,7 +69,7 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+engine = create_engine(sqlite_url, connect_args=connect_args)
 
 
 def init_db_and_tables():
@@ -103,7 +103,7 @@ def init_db_and_tables():
             init_todo = init_todos[i]
             init_todo_user_id = init_todo_user_ids[i % 4]
             user = session.exec(select(User).where(User.id == init_todo_user_id)).first()
-            todo = Todo(item=init_todo, plan_time=None, user_id=init_todo_user_id, user=user)  # type: ignore
+            todo = Todo(item=init_todo, plan_time=datetime.now(), user_id=init_todo_user_id, user=user)  # type: ignore
             session.add(todo)
         session.commit()
 
@@ -118,8 +118,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-# TODO: add return type for each function
 
 
 @app.post("/create_todos/", tags=['todo'])
@@ -177,7 +175,7 @@ def update_todos(updateDto: UpdateTodoDto, todo_id: int) -> Todo:
 
 
 @app.post("/create_users/", tags=['user'])
-def create_user(userDto: UserDto):
+def create_user(userDto: UserDto) -> User:
     with Session(engine) as session:
         user = User(user_name=userDto.user_name, pwd=userDto.pwd)
         session.add(user)  # no id in user, because it has not been saved yet
@@ -194,18 +192,20 @@ def delete_user(user_id: int):
         user: User = results.one()
         session.delete(user)
 
-# TODO: return in paginate model
-
 
 @app.get("/get_users/", tags=['user'])
 # page: int, per_page: int
-def read_users():
+def read_users(page: int, per_page: int) -> PaginateModel[User]:
+    skip = (page - 1) * per_page
+    limit = per_page
     with Session(engine) as session:
         # use offset and limit to create pagination
-        statement = select(User)
+        count_statement = select(func.count(User.user_name))  # type: ignore
+        statement = select(User).offset(skip).limit(limit)
         result = session.exec(statement)
-        users = result.all()
-        return users
+        total_items = session.exec(count_statement).one()
+        items = result.all()
+        return PaginateModel[User](page=page, items=items, per_page=per_page, total_items=total_items)
 
 
 @app.post("/update_users/{user_id}", tags=['user'])
@@ -222,43 +222,64 @@ def update_user(user_id: int, userDto: UserDto):
 
 
 @app.get("/get_user_by_todo/{todo_id}", tags=['apis'])
-def get_user_by_todo(todo_id: int):
+def get_user_by_todo(todo_id: int) -> User:
     with Session(engine) as session:
         statement = select(Todo).where(Todo.id == todo_id)
         results = session.exec(statement)
         return results.one().user
 
-# TODO: use Todo to get user_id = user_id and use offset and limit to paginate
-# TODO: return in paginate model
-
 
 @app.get("/get_todos_by_user/{user_id}", tags=['apis'])
-def read_todos_by_user(user_id: int):
+def read_todos_by_user(page: int, per_page: int, user_id: int) -> PaginateModel[Todo]:
+    skip = (page - 1) * per_page
+    limit = per_page
     with Session(engine) as session:
-        user = session.exec(select(User).where(User.id == user_id)).first()
-        if not user:
+        count_statement = select(func.count(Todo.user_id)).where(Todo.user_id == user_id)  # type: ignore
+        total_items = session.exec(count_statement).one()
+        todos = session.exec(select(Todo).where(Todo.user_id == user_id).offset(skip).limit(limit))
+        items = todos.all()
+        if not todos:
             raise HTTPException(status_code=400, detail='User not found!')
-        return user.todo_list
+        return PaginateModel[Todo](page=page, items=items, per_page=per_page, total_items=total_items)
 
 
-# TODO: return in paginate model
-@app.get("/get_todos_by_item_name/{item_name}", tags=['apis'])
-def get_todos_by_item_name(item_name: str):
+@app.get("/get_todos_by_item_name/{item_name}", tags=['apis'], description="Get todos by the item name")
+def get_todos_by_item_name(item_name: str, page: int, per_page: int) -> PaginateModel[Todo]:
+    skip = (page - 1) * per_page
+    limit = per_page
     with Session(engine) as session:
-        statement = select(Todo).where(Todo.item == item_name)
-        results = session.exec(statement).all()
-        return results
+        count_statement = select(func.count(Todo.id)).where(Todo.item.like('%'+item_name+'%'))  # type: ignore
+        total_items = session.exec(count_statement).one()
+        statement = select(Todo).where(Todo.item.like('%'+item_name+'%')).offset(skip).limit(limit)  # type: ignore
+        items = session.exec(statement).all()
+        return PaginateModel[Todo](page=page, items=items, per_page=per_page, total_items=total_items)
 
-# TODO: return in paginate model
 
-
-@app.get("/get_todos_by_plan_time/{plan_time}", tags=['apis'])
-def get_todo_by_plan_time(plan_time: str):
+@app.get("/get_todos_by_plan_time/{plan_time_str}", tags=['apis'], description="Get todos by the plan time")
+def get_todo_by_plan_time(plan_time_str: str, page: int, per_page: int) -> PaginateModel[Todo]:
+    skip = (page - 1) * per_page
+    limit = per_page
+    total_items = []
     with Session(engine) as session:
-        if (plan_time == "null"):
+
+        # TODO: front end must set null when empty
+        if (plan_time_str == "null"):
+            count_statement = select(func.count(Todo.id)).where(Todo.plan_time == None)  # type: ignore
+            total_items = session.exec(count_statement).one()
             statement = select(Todo).where(Todo.plan_time == None)
             results = session.exec(statement).all()
-            return results
-        statement = select(Todo).where(Todo.plan_time == plan_time)
+            return PaginateModel[Todo](page=page, items=results, per_page=per_page, total_items=total_items)
+        if len(plan_time_str) == len('2024-06-06 11'):
+            plan_time_start = datetime.strptime(plan_time_str, "%Y-%m-%d %H")
+            plan_time_end = plan_time_start + timedelta(hours=1)
+        elif len(plan_time_str) == len('2024-06-06'):
+            plan_time_start = datetime.strptime(plan_time_str, "%Y-%m-%d")
+            plan_time_end = plan_time_start + timedelta(days=1)
+        else:
+            raise HTTPException(status_code=400, detail="Plan time format invalid!")
+        query = and_(Todo.plan_time > plan_time_start, Todo.plan_time < plan_time_end)  # type: ignore
+        count_statement = select(func.count(Todo.id)).where(query)  # type: ignore
+        total_items = session.exec(count_statement).one()
+        statement = select(Todo).where(query)  # type: ignore
         results = session.exec(statement).all()
-        return results
+        return PaginateModel[Todo](page=page, items=results, per_page=per_page, total_items=total_items)
